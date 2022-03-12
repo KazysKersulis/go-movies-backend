a57ef8f4aa15eb429dc73bf91b6b72ce0e41343e
Kazys Keršulis <kazimieras.kersulis@gmail.com>
2022-03-12 17:56:37 +0200

    get movies by genre

--- cmd/api/movie-handlers.go
+++ cmd/api/movie-handlers.go
@@ -20,10 +20,15 @@ func (app *application) getOneMovie(w http.ResponseWriter, r *http.Request) {
 	}
 
 	movie, err := app.models.DB.Get(id)
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
 
 	err = app.writeJSON(w, http.StatusOK, movie, "movie")
 	if err != nil {
-
+		app.errorJSON(w, err)
+		return
 	}
 }
 
@@ -51,6 +56,29 @@ func (app *application) getAllGenres(w http.ResponseWriter, r *http.Request) {
 	}
 
 	err = app.writeJSON(w, http.StatusOK, genres, "genres")
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+}
+
+func (app *application) getAllMoviesByGenre(w http.ResponseWriter, r *http.Request) {
+	params := httprouter.ParamsFromContext(r.Context())
+
+	genreID, err := strconv.Atoi(params.ByName("genre_id"))
+	if err != nil {
+		app.logger.Println(errors.New("invalid id parameter"))
+		app.errorJSON(w, err)
+		return
+	}
+
+	movies, err := app.models.DB.All(genreID)
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	err = app.writeJSON(w, http.StatusOK, movies, "movies")
 	if err != nil {
 		app.errorJSON(w, err)
 		return
