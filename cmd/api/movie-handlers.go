804c976b537788b4dcb07c9a791d24c480155dcc
Kazys Keršulis <kazimieras.kersulis@gmail.com>
2022-04-02 18:00:39 +0300

    update movie

--- cmd/api/movie-handlers.go
+++ cmd/api/movie-handlers.go
@@ -116,6 +116,22 @@ func (app *application) editMovie(w http.ResponseWriter, r *http.Request) {
 
 	var movie models.Movie
 
+	if payload.ID != "0" {
+		id, err := strconv.Atoi(payload.ID)
+		if err != nil {
+			app.errorJSON(w, err)
+			return
+		}
+
+		m, err := app.models.DB.Get(id)
+		if err != nil {
+			app.errorJSON(w, err)
+			return
+		}
+		movie = *m
+		movie.UpdatedAt = time.Now()
+	}
+
 	movie.ID, _ = strconv.Atoi(payload.ID)
 	movie.Title = payload.Title
 	movie.Description = payload.Description
@@ -127,10 +143,18 @@ func (app *application) editMovie(w http.ResponseWriter, r *http.Request) {
 	movie.CreatedAt = time.Now()
 	movie.UpdatedAt = time.Now()
 
-	err = app.models.DB.InsertMovie(movie)
-	if err != nil {
-		app.errorJSON(w, err)
-		return
+	if movie.ID == 0 {
+		err = app.models.DB.InsertMovie(movie)
+		if err != nil {
+			app.errorJSON(w, err)
+			return
+		}
+	} else {
+		err = app.models.DB.UpdateMovie(movie)
+		if err != nil {
+			app.errorJSON(w, err)
+			return
+		}
 	}
 
 	ok := jsonResp{
